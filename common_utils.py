e6c3907be02cb1066d76666b306b240f57902564
andrew20012656 <andrew20012656@gmail.com>
2023-11-19 20:34:59 -0500

    updated common_utils.py

--- common_utils.py
+++ common_utils.py
@@ -1,20 +1,27 @@
 from datetime import datetime
 
+
 def convert_to_timestamp(time_str: str):
     """
     Return a POSIX timestamp 
 
     Parameters:
-    - time_str (str): a string representing a timestamp following ISO 8601 Standard
+    - `time_str` (str): a string representing a datetime following the following formats:
+        - "%Y-%m-%dT%H:%M:%S.%fZ"
+        - "%Y-%m-%dT%H:%M:%SZ"
+        - "%Y:%m:%d %H:%M:%S"
+        - "%Y%m%dT%H%M%S.%fZ"
     """
-    date_formats = ["%Y-%m-%dT%H:%M:%S.%fZ", "%Y-%m-%dT%H:%M:%SZ", "%Y:%m:%d %H:%M:%S", "%Y%m%dT%H%M%S.%fZ"]
-    start_datetime = None
+    date_formats = ["%Y-%m-%dT%H:%M:%S.%fZ", "%Y-%m-%dT%H:%M:%SZ",
+                    "%Y:%m:%d %H:%M:%S", "%Y%m%dT%H%M%S.%fZ"]
+    datetime_obj = None
     for date_format in date_formats:
         try:
-            start_datetime = datetime.strptime(time_str, date_format)
+            datetime_obj = datetime.strptime(time_str, date_format)
             break
         except ValueError:
             continue
-    
-    print(start_datetime)
-    return start_datetime.timestamp()
+    if datetime is None:
+        print("The input Datetime format is not recognized")
+    else:
+        return datetime_obj.timestamp()
